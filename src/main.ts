3df48a06c1facce5b9f018ce720cd187099a62ce
yutailang0119 <muta.yutaro@gmail.com>
2020-11-01 00:31:49 +0900

    Fix main to use parser and command

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import fs from 'fs'
+import {parseReport} from './parser'
+import {echoMessages} from './command'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const reportPath = core.getInput('json_path', {required: true})
+    const json = fs.readFileSync(reportPath, 'utf-8')
+    const annotations = await parseReport(json)
+    await echoMessages(annotations)
   } catch (error) {
     core.setFailed(error.message)
   }
