d8908aee7ec04b078e8da0c3c060a72dc7b2ff8e
yutailang0119 <muta.yutaro@gmail.com>
2020-11-01 00:31:20 +0900

    Add command.ts to pipe actions workflow command

---
+++ src/command.ts
@@ -0,0 +1,20 @@
+import * as command from '@actions/core/lib/command'
+import {Annotation} from './annotation'
+
+const commandProperties = (annotation: Annotation): {[key: string]: string} => {
+  return {
+    file: annotation.path,
+    line: `${annotation.line}`,
+    col: `${annotation.column}`
+  }
+}
+
+export async function echoMessages(annotations: Annotation[]): Promise<void> {
+  for (const annotation of annotations) {
+    command.issueCommand(
+      annotation.severityLevel,
+      commandProperties(annotation),
+      annotation.message
+    )
+  }
+}
