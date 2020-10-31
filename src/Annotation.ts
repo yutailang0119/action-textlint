8c1fad25fd90b7041ef23dbbb9f4e83ff73963f3
yutailang0119 <muta.yutaro@gmail.com>
2020-11-01 00:22:17 +0900

    Add Annotation and AnnotationSeverityLevel

---
+++ src/Annotation.ts
@@ -0,0 +1,23 @@
+import {AnnotationSeverityLevel} from './AnnotationSeverityLevel'
+
+export class Annotation {
+  severityLevel: AnnotationSeverityLevel
+  path: string
+  line: number
+  column: number
+  message: string
+
+  constructor(
+    severity: number,
+    path: string,
+    line: number,
+    column: number,
+    message: string
+  ) {
+    this.severityLevel = severity === 2 ? 'error' : 'warning'
+    this.path = path
+    this.line = line
+    this.column = column
+    this.message = message
+  }
+}
