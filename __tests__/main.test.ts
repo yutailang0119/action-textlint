4aee43f49df4adf6e361aef1879641a5bf81b162
yutailang0119 <muta.yutaro@gmail.com>
2020-11-01 13:54:51 +0900

    Fix main.ts to support raw json input

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,12 +3,37 @@ import * as cp from 'child_process'
 import * as path from 'path'
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
+test('test runs with json file', () => {
   process.env['INPUT_JSON_PATH'] = path.join(
     __dirname,
     'resource',
     'textlint-report.json'
   )
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
+
+test('test runs with textlint output', () => {
+  const json = `[
+    {
+      "messages":[
+        {
+          "type":"lint",
+          "ruleId":"sample-rule/no-weak-phrase",
+          "message":"adverbs can weaken meaning",
+          "index":12,
+          "line":3,
+          "column":6,
+          "severity":1
+        }
+      ],
+      "filePath":"Foo.md"
+    }
+  ]`
+  process.env['INPUT_TEXTLINT_OUTPUT'] = json
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,14 @@ import {echoMessages} from './command'
 
 async function run(): Promise<void> {
   try {
-    const reportPath = core.getInput('json_path', {required: true})
-    const json = fs.readFileSync(reportPath, 'utf-8')
+    let json: string
+    const textlintOutput = core.getInput('textlint_output', {required: false})
+    if (textlintOutput === '') {
+      const jsonPath = core.getInput('json_path', {required: false})
+      json = fs.readFileSync(jsonPath, 'utf-8')
+    } else {
+      json = textlintOutput
+    }
     const annotations = await parseReport(json)
     await echoMessages(annotations)
   } catch (error) {
