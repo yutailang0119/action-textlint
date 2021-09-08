3e09adb86d0443e0024b7d2e1af81a4f78597d27
yutailang0119 <yutailang0119@gmail.com>
2021-09-09 01:27:16 +0900

    Fix parser to replace regular function with arrow function

--- src/parser.ts
+++ src/parser.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import {TextlintResult} from '@textlint/types/src/Message/TextlintResult'
 import {Annotation} from './annotation'
 
-export async function parseReport(json: string): Promise<Annotation[]> {
+export const parseReport = async (json: string): Promise<Annotation[]> => {
   const files: TextlintResult[] = JSON.parse(json)
   return new Promise(resolve => {
     try {
