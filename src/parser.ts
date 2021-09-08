8a77c6d91d7d24da5a73fd65b7ce43179801aaef
yutailang0119 <yutailang0119@gmail.com>
2021-09-09 02:41:03 +0900

    Fix parserReport to be not handle error

--- src/parser.ts
+++ src/parser.ts
@@ -1,25 +1,20 @@
-import * as core from '@actions/core'
 import {TextlintResult} from '@textlint/types/src/Message/TextlintResult'
 import {Annotation} from './annotation'
 
 export const parseReport = (json: string): Annotation[] => {
   const files: TextlintResult[] = JSON.parse(json)
   const annotations: Annotation[] = []
-  try {
-    for (const file of files) {
-      for (const message of file.messages) {
-        const annotation = new Annotation(
-          message.severity,
-          `${message.message} (${message.ruleId})`,
-          file.filePath,
-          message.line,
-          message.column
-        )
-        annotations.push(annotation)
-      }
+  for (const file of files) {
+    for (const message of file.messages) {
+      const annotation = new Annotation(
+        message.severity,
+        `${message.message} (${message.ruleId})`,
+        file.filePath,
+        message.line,
+        message.column
+      )
+      annotations.push(annotation)
     }
-  } catch (error) {
-    core.debug(`failed to read ${error}`)
   }
   return annotations
 }
