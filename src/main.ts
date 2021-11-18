0c04c982b984f2f49632299909ed409228140233
yutailang0119 <yutailang0119@gmail.com>
2021-11-18 22:32:36 +0900

    Fix inputs to kebab-case

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import {parseReport} from './parser'
 async function run(): Promise<void> {
   try {
     let json: string
-    const textlintOutput = core.getInput('textlint_output', {required: false})
+    const textlintOutput = core.getInput('textlint-output', {required: false})
     if (textlintOutput === '') {
-      const jsonPath = core.getInput('json_path', {required: false})
+      const jsonPath = core.getInput('json-path', {required: false})
       json = fs.readFileSync(jsonPath, 'utf-8')
     } else {
       json = textlintOutput
