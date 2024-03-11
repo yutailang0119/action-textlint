e1c86e2253a36c353a34571cb22e6a2a29331a8a
yutailang0119 <yutailang0119@gmail.com>
2024-03-11 23:59:11 +0900

    Fix parse.test

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -2,50 +2,51 @@ import {expect, test} from '@jest/globals'
 import {Annotation} from '../src/annotation'
 import {parseReport} from '../src/parser'
 
+const json = `[
+  {
+    "messages":[
+      {
+        "type":"lint",
+        "ruleId":"sample-rule/no-weak-phrase",
+        "message":"adverbs can weaken meaning",
+        "index":12,
+        "line":3,
+        "column":6,
+        "severity":1
+      },
+      {
+        "type":"lint",
+        "ruleId":"sample-rule/misspellings",
+        "message":"This is a commonly misspelled word. Correct it to useful",
+        "index":13,
+        "line":22,
+        "column":7,
+        "severity":2
+      }
+    ],
+    "filePath":"Foo.md"
+  },
+  {
+    "messages":[
+      {
+        "type":"lint",
+        "ruleId":"sample-rule/sentence:uppercase",
+        "message":"sentence should start with an uppercase letter",
+        "index":7,
+        "line":3,
+        "column":1,
+        "severity":2,
+        "fix":{
+          "range":[1,4],
+          "text":"This"
+        }
+      }
+    ],
+    "filePath":"Bar.md"
+  }
+]`
+
 test('test parse', () => {
-  const json = `[
-    {
-      "messages":[
-        {
-          "type":"lint",
-          "ruleId":"sample-rule/no-weak-phrase",
-          "message":"adverbs can weaken meaning",
-          "index":12,
-          "line":3,
-          "column":6,
-          "severity":1
-        },
-        {
-          "type":"lint",
-          "ruleId":"sample-rule/misspellings",
-          "message":"This is a commonly misspelled word. Correct it to useful",
-          "index":13,
-          "line":22,
-          "column":7,
-          "severity":2
-        }
-      ],
-      "filePath":"Foo.md"
-    },
-    {
-      "messages":[
-        {
-          "type":"lint",
-          "ruleId":"sample-rule/sentence:uppercase",
-          "message":"sentence should start with an uppercase letter",
-          "index":7,
-          "line":3,
-          "column":1,
-          "severity":2,
-          "fix":{
-            "range":[1,4],
-            "text":"This"
-          }
-        }
-      ],
-      "filePath":"Bar.md"
-    }
-  ]`
   const annotation1 = new Annotation(
     1,
     'adverbs can weaken meaning (sample-rule/no-weak-phrase)',
@@ -68,5 +69,9 @@ test('test parse', () => {
     1
   )
 
-  expect(parseReport(json, false)).toEqual([annotation1, annotation2, annotation3])
+  expect(parseReport(json, false)).toEqual([
+    annotation1,
+    annotation2,
+    annotation3
+  ])
 })
