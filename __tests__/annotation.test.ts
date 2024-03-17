9c113372255fc16ebba118e4c084ccf50c69fc3b
yutailang0119 <yutailang0119@gmail.com>
2024-03-17 23:46:06 +0900

    Fix annotation.test to loc

--- __tests__/annotation.test.ts
+++ __tests__/annotation.test.ts
@@ -6,8 +6,16 @@ test('test Annotation.constructor with warning', () => {
     1,
     'adverbs can weaken meaning (sample-rule/no-weak-phrase)',
     'Foo.md',
-    3,
-    6
+    {
+      start: {
+        line: 3,
+        column: 6
+      },
+      end: {
+        line: 3,
+        column: 7
+      }
+    }
   )
   expect(annotation.severityLevel).toEqual('warning')
   expect(annotation.message).toEqual(
@@ -16,7 +24,9 @@ test('test Annotation.constructor with warning', () => {
   expect(annotation.properties).toEqual({
     file: 'Foo.md',
     startLine: 3,
-    startColumn: 6
+    startColumn: 6,
+    endLine: 3,
+    endColumn: 7
   })
 })
 
@@ -25,8 +35,16 @@ test('test Annotation.constructor with error', () => {
     2,
     'This is a commonly misspelled word. Correct it to useful (sample-rule/misspellings)',
     'Foo.md',
-    22,
-    7
+    {
+      start: {
+        line: 22,
+        column: 7
+      },
+      end: {
+        line: 22,
+        column: 8
+      }
+    }
   )
   expect(annotation.severityLevel).toEqual('error')
   expect(annotation.message).toEqual(
@@ -35,17 +53,28 @@ test('test Annotation.constructor with error', () => {
   expect(annotation.properties).toEqual({
     file: 'Foo.md',
     startLine: 22,
-    startColumn: 7
+    startColumn: 7,
+    endLine: 22,
+    endColumn: 8
   })
 })
 
 test('test Annotation.constructor with other', () => {
-  const annotation1 = new Annotation(NaN, '', 'Foo.md', 0, 0)
+  const annotation1 = new Annotation(NaN, '', 'Foo.md', {
+    start: {line: 0, column: 0},
+    end: {line: 0, column: 1}
+  })
   expect(annotation1.severityLevel).toEqual('warning')
 
-  const annotation2 = new Annotation(0, '', 'Bar.md', 0, 0)
+  const annotation2 = new Annotation(0, '', 'Bar.md', {
+    start: {line: 0, column: 0},
+    end: {line: 0, column: 1}
+  })
   expect(annotation2.severityLevel).toEqual('warning')
 
-  const annotation3 = new Annotation(3, '', 'Baz.md', 0, 0)
+  const annotation3 = new Annotation(3, '', 'Baz.md', {
+    start: {line: 0, column: 0},
+    end: {line: 0, column: 1}
+  })
   expect(annotation3.severityLevel).toEqual('warning')
 })
