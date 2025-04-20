6808db8aedec25bf760bd9d1340efc4f730ea771
yutailang0119 <yutailang0119@gmail.com>
2025-04-20 23:34:22 +0900

    Add src files

---
+++ src/parser.ts
@@ -0,0 +1,35 @@
+import {TextlintResult} from '@textlint/types/src/Message/TextlintResult'
+import {Annotation} from './annotation'
+
+export const parseReport = (
+  json: string,
+  ignoreWarnings: boolean
+): Annotation[] => {
+  const results: TextlintResult[] = JSON.parse(json)
+  const annotations: Annotation[] = results.flatMap(result => {
+    return result.messages.map(message => {
+      return new Annotation(
+        message.severity,
+        `${message.message} (${message.ruleId})`,
+        result.filePath,
+        {
+          start: {
+            line: message.loc.start.line,
+            column: message.loc.start.column
+          },
+          end: {
+            line: message.loc.end.line,
+            column: message.loc.end.column
+          }
+        }
+      )
+    })
+  })
+  if (ignoreWarnings === true) {
+    return annotations.filter(annotation => {
+      return annotation.severityLevel !== 'warning'
+    })
+  } else {
+    return annotations
+  }
+}
